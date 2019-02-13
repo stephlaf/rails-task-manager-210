3ec708d667bc62d15d2acdede046a446e1f85668
Stephane Lafontaine <stephane@medqigong.ca>
2019-02-13 16:49:21 -0500

    Done, painfully

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,17 +3,31 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
   end
+
   def new
+    @task = Task.new
   end
+
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
+
   def edit
   end
+
   def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
+
   def destroy
+    @task.delete
+    redirect_to tasks_path
   end
 
   private
